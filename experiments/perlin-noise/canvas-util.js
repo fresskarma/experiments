431de64a481fd38b0fdaae720f0055d9b02399a8
Lucas Jenss <lucas@x3ro.de>
2012-08-30 19:02:34 +0200

    Site updated at 2012-08-30 17:02:34 UTC

---
+++ experiments/perlin-noise/canvas-util.js
@@ -0,0 +1,88 @@
+var Canvas;
+
+;(function($) {
+
+Canvas = function (canvasId) {
+    this.canvasId = canvasId;
+    this.canvas = document.getElementById(this.canvasId);
+    this.ctx = this.canvas.getContext('2d');
+}
+
+Canvas.prototype = {
+
+    drawWithFunction: function (drawFunction, optionsContainer) {
+        var options = {};
+        var that = this;
+
+        var f = function() {
+            drawFunction.call(that, that.ctx, that.canvas.width, that.canvas.height, that.determineOptions(optionsContainer));
+        }
+
+        this.bindOptionChangeEvents(optionsContainer, f);
+
+        f();
+    },
+
+    bindOptionChangeEvents: function(optionsContainer, changeFunc) {
+        if(typeof optionsContainer === "undefined") {
+            return;
+        }
+
+        $(optionsContainer).find('input[type=checkbox]').each(function() {
+            var obj = $(this);
+            obj.click(changeFunc);
+        });
+
+        $(optionsContainer).find('select, input').each(function() {
+            var obj = $(this);
+            obj.change(changeFunc);
+        });
+
+    },
+
+    determineOptions: function(optionsContainer) {
+        if(typeof optionsContainer === "undefined") {
+            return {};
+        }
+
+        var options = {};
+        $(optionsContainer).find('input[type=checkbox]').each(function() {
+            var obj = $(this);
+            options[obj.attr('name')] = obj.prop('checked');
+        });
+
+        $(optionsContainer).find('select').each(function() {
+            var obj = $(this);
+            options[obj.attr('name')] = obj.val();
+        });
+
+        return options;
+    },
+
+    clear: function (ctx) {
+        ctx.save();
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        ctx.restore();
+    },
+
+    connectTheDots: function (dots, interpolationFunction) {
+        for(var d = 0; d < (dots.length - 1); d++) {
+            var a = dots[d];
+            var b = dots[d + 1];
+            var lastY = a[1];
+            for(var i = 0; i <= 20; i++) {
+                var y = interpolationFunction(a[1], b[1], i / 20.0);
+                this.ctx.beginPath();
+                this.ctx.moveTo(a[0] + i, lastY);
+                this.ctx.lineTo(a[0] + i + 1, y);
+                this.ctx.stroke();
+                lastY = y;
+            }
+        }
+    }
+
+};
+
+
+})(jQuery);
---
+++ experiments/perlin-noise/perlin.js
@@ -0,0 +1,59 @@
+function linear_interpolate(a, b, x) {
+    return a * (1 - x) + b * x;
+}
+
+function cosine_interpolate(a, b, x) {
+    ft = x * Math.PI
+    f = (1 - Math.cos(ft)) * 0.5
+    return a * (1 - f) + b * f
+}
+
+function smooth(dots) {
+    for(var i = 1; i < (dots.length - 1); i++) {
+        dots[i][1] = (dots[i - 1][1] + dots[i][1] + dots[i + 1][1]) / 3.0
+    }
+}
+
+
+
+function drawInterpolation(ctx, width, height, options) {
+    Math.seedrandom("Foo");
+
+    this.clear(ctx);
+
+    var x;
+    var dots = [];
+
+    for(x = 0; x < width; x += 20) {
+        var y = Math.random() * height;
+        dots.push([x, y]);
+    }
+
+    if(options.smooth) {
+        smooth(dots);
+    }
+
+    for(i = 0; i < dots.length; i++) {
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.fillRect(dots[i][0], dots[i][1], 4, 4);
+    }
+
+
+    switch(options.interpolation) {
+        case 'linear':
+            this.connectTheDots(dots, linear_interpolate);
+            break;
+        case 'cosine':
+            this.connectTheDots(dots, cosine_interpolate);
+            break;
+    }
+
+    ctx.restore();
+
+}
+
+function init() {
+    new Canvas("linearInterpolationNoise").drawWithFunction(drawInterpolation, '#linearInterpolationOptions');
+}
+
+document.addEventListener("DOMContentLoaded", init, false);
